267011130d10249df2d6ac9459a940f0290c3951
srive <sri.venkatraghavan@gmail.com>
2021-01-23 15:14:40 +0530

    Implemented basic ldap with spring security

---
+++ src/main/java/com/example/SpringSecurityLDAP/controller/HomeController.java
@@ -0,0 +1,23 @@
+package com.example.SpringSecurityLDAP.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+	@RequestMapping("/")
+	public String home() {
+		return ("<h1>Welcome Home</h1>");
+	}
+
+	@RequestMapping("/user")
+	public String user() {
+		return ("<h1>Welcome User</h1>");
+	}
+
+	@RequestMapping("/admin")
+	public String admin() {
+		return ("<h1>Welcome Admin</h1>");
+	}
+}
